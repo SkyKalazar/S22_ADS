b1a5834317df34c1ee06e6d85f8b443a79326888
SkyKalazar <74299324+SkyKalazar@users.noreply.github.com>
2022-04-22 13:17:28 +0100

    Seemingly working insert for the BST

--- BalancedBinarySearchTree/src/Tree/BinarySearchTree.java
+++ BalancedBinarySearchTree/src/Tree/BinarySearchTree.java
@@ -13,19 +13,25 @@ public class BinarySearchTree<E> extends BinaryTree<E> {
         if(contains(newElement)) {
             return false;
         }
-        insertRecursively((BinarySearchTreeNode<E>)getRoot(), newElement);
+        insertRecursively((BinarySearchTreeNode<E>)getRoot() , newElement);
         return true;
     }
     private void insertRecursively(BinarySearchTreeNode<E> targetNode, E newElement) {
-        if(targetNode == null) {
-           targetNode = new BinarySearchTreeNode<>(newElement);
-           return;
-        }
         if(targetNode.compareTo(newElement) > 0) {
-            insertRecursively((BinarySearchTreeNode<E>) targetNode.getLeftChild(), newElement);
+            if(targetNode.getLeftChild() == null) {
+                targetNode.addLeftChild(new BinarySearchTreeNode<>(newElement));
+            }
+            else {
+                insertRecursively((BinarySearchTreeNode<E>)targetNode.getLeftChild(), newElement);
+            }
         }
-        if(targetNode.compareTo(newElement) <= 0) {
-            insertRecursively((BinarySearchTreeNode<E>)targetNode.getRightChild(), newElement);
+        else if(targetNode.compareTo(newElement) <= 0) {
+            if(targetNode.getRightChild() == null) {
+                targetNode.addRightChild(new BinarySearchTreeNode<>(newElement));
+            }
+            else {
+                insertRecursively((BinarySearchTreeNode<E>)targetNode.getRightChild(), newElement);
+            }
         }
     }
     public boolean removeElement(BinarySearchTreeNode<E> targetElement) {
