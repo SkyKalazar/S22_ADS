8d7508cf103ac68decda8587aaff2b7861ba48d8
SkyKalazar <74299324+SkyKalazar@users.noreply.github.com>
2022-04-24 18:29:20 +0100

    FrameForTheRod
    Beginnings of the rod exercise

---
+++ RodSplitter/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Rod guineaPig = new Rod();
+
+        System.out.println(guineaPig.calculateMaximumWorth(15));
+    }
+}
--- RodSplitter/src/Rod.java
+++ RodSplitter/src/Rod.java
@@ -1,2 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.util.Map.entry;
+
 public class Rod {
+
+    private static final Map<Integer, Integer> segmentValues = Map.ofEntries(entry(1,7), entry(2,5),
+            entry(3,8), entry(4,9), entry(5,10), entry(6,17), entry(7,17), entry(8,20),
+            entry(9,24), entry(10,30));
+
+    public Rod() {
+
+    }
+
+    public int calculateMaximumWorth(int rodLength) {
+        int max = 0;
+
+        while(rodLength != 0) {
+            int targetSegment = rodLength%10;
+            max += segmentValues.get(targetSegment);
+            rodLength -= targetSegment;
+        }
+        return max;
+    }
 }
