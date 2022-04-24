4b43a2c69f222a594f56bfa5aefe7c378da74745
SkyKalazar <74299324+SkyKalazar@users.noreply.github.com>
2022-04-24 19:13:20 +0100

    BasicRodSplitter
    Rod splitter implementation with simple brute force

--- RodSplitter/src/Main.java
+++ RodSplitter/src/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
         Rod guineaPig = new Rod();
 
-        System.out.println(guineaPig.calculateMaximumWorth(15));
+        System.out.println(guineaPig.rodRequestReceiver(11));
     }
 }
--- RodSplitter/src/Rod.java
+++ RodSplitter/src/Rod.java
@@ -5,22 +5,53 @@ import static java.util.Map.entry;
 
 public class Rod {
 
-    private static final Map<Integer, Integer> segmentValues = Map.ofEntries(entry(1,7), entry(2,5),
-            entry(3,8), entry(4,9), entry(5,10), entry(6,17), entry(7,17), entry(8,20),
-            entry(9,24), entry(10,30));
-
+    private HashMap<Integer, Integer> segmentValues;
     public Rod() {
+        segmentValues = new HashMap<>();
+
+        segmentValues.put(0, 0);
+        segmentValues.put(1, 1);
+        segmentValues.put(2, 5);
+        segmentValues.put(3, 8);
+        segmentValues.put(4, 9);
+        segmentValues.put(5, 10);
+        segmentValues.put(6, 17);
+        segmentValues.put(7, 17);
+        segmentValues.put(8, 20);
+        segmentValues.put(9, 24);
+        segmentValues.put(10, 30);
 
     }
 
-    public int calculateMaximumWorth(int rodLength) {
-        int max = 0;
+    public int rodRequestReceiver(int totalRodLength) {
 
-        while(rodLength != 0) {
-            int targetSegment = rodLength%10;
-            max += segmentValues.get(targetSegment);
-            rodLength -= targetSegment;
+        int finalMax = 0;
+
+        while(totalRodLength != 0) {
+            if(totalRodLength > 10) {
+            finalMax += calculateMaximumWorth(10);
+            totalRodLength -= 10;
+            }
+            else {
+                finalMax += calculateMaximumWorth(totalRodLength);
+                totalRodLength = 0;
+            }
         }
-        return max;
+        return finalMax;
+    }
+
+    public int calculateMaximumWorth(int localRodLength) {
+
+        int localMax = 0;
+
+        //let's say the call is 15
+
+        for(int i = 0; i < localRodLength; i++) {
+            if(segmentValues.get(i) + segmentValues.get(localRodLength-i) > localMax) {
+                localMax = segmentValues.get(i) + segmentValues.get(localRodLength-i);
+            }
+        }
+
+        return localMax;
     }
 }
